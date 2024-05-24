96440293c206715981413ca6cfce85cb8e6608fe
Alex Ong <aong3@woolworths.com.au>
2024-05-24 12:09:16 +1000

    Initial commit

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  cloud {
+    organization = "aong3-wooliesx"
+    workspaces {
+      name = "web-network-dev"
+    }
+  }
+}
---
+++ imports.tf
@@ -0,0 +1,49 @@
+##################################################################################
+# IMPORTS
+##################################################################################
+/*
+import {
+  to = module.main.aws_vpc.this[0]
+  id = "vpc-0cf887fae2cb34d81" #VPC
+}
+
+import {
+  to = module.main.aws_subnet.public[0]
+  id = "subnet-0907bca2afcaf879f" #PublicSubnet1
+}
+
+import {
+  to = module.main.aws_subnet.public[1]
+  id = "subnet-01e9e6ec1beb38ecf" #PublicSubnet2
+}
+
+import {
+  to = module.main.aws_internet_gateway.this[0]
+  id = "igw-0654b7846a88555fe" #InternetGateway
+}
+
+import {
+  to = module.main.aws_route.public_internet_gateway[0]
+  id = "rtb-08e41cf12340ee5ef_0.0.0.0/0" #DefaultPublicRoute
+}
+
+import {
+  to = module.main.aws_route_table.public[0]
+  id = "rtb-08e41cf12340ee5ef" #PublicRouteTable
+}
+
+import {
+  to = module.main.aws_route_table_association.public[0]
+  id = "subnet-0907bca2afcaf879f/rtb-08e41cf12340ee5ef" #PublicSubnet1/PublicRouteTable
+}
+
+import {
+  to = module.main.aws_route_table_association.public[1]
+  id = "subnet-01e9e6ec1beb38ecf/rtb-08e41cf12340ee5ef" #PublicSubnet2/PublicRouteTable
+}
+
+import {
+  to = aws_security_group.ingress
+  id = "sg-0e8506f72abeaf025" #NoIngressSecurityGroup
+}
+*/
